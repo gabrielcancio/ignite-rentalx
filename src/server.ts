85c06dfd584a6dcd63c056ead59fed4cad31c6c1
gabrielcancio <gabriel.cancio02@gmail.com>
2021-04-16 22:48:39 -0300

    feat: Adding Categories repository

--- src/server.ts
+++ src/server.ts
@@ -4,22 +4,8 @@ import categoriesRoutes from "./routes/categories.routes";
 const app = express();
 
 app.use(express.json());
-app.use(categoriesRoutes);
 
-app.get("/", (request, response) => {
-  const data = {
-    app: "Rentalx",
-    platform: "nodejs",
-  };
-
-  return response.json({ data });
-});
-
-app.post("/courses", (request, response) => {
-  const name = request.body;
-
-  return response.json({ name });
-});
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () =>
   console.log("Server is running at http://localhost:3333")
