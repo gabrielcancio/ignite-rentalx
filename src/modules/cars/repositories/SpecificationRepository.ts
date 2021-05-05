6b6314b54a86267904ca70426888f9f1902ce0c7
gabrielcancio <gabriel.cancio02@gmail.com>
2021-05-05 08:26:18 -0300

    feat: Adding specification service

---
+++ src/modules/cars/repositories/SpecificationRepository.ts
@@ -0,0 +1,9 @@
+import ISpecificationRepository from "./ISpecificationRepository";
+
+class SpecificationRepository implements ISpecificationRepository {
+  create(): void {
+    console.log("To do!!");
+  }
+}
+
+export default SpecificationRepository;
---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,9 @@
+import Specification from "../models/Specification";
+
+class CreateSpecificationService {
+  execute(): void {
+    console.log("Method not implemented!!", Specification);
+  }
+}
+
+export default CreateSpecificationService;
